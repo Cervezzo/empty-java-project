3d3752e748c3468ab47742640be26fd38a998d9a
Antonio J. Castillo <antoniocasma@gmail.com>
2018-04-18 20:15:23 +0200

    Ejercicios del primer y segundo día.

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -1,6 +1,8 @@
 package com.teaching.android;
 
 
+import javax.swing.*;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -10,6 +12,7 @@ public class JavaApp {
 
     public static void main(String[] args) {
 
+        /*
         Persona p1 = new Persona("Paco", 200f);
         Persona p2 = new Persona("Juan", 400f);
             System.out.println();
@@ -20,11 +23,58 @@ public class JavaApp {
             System.out.println();
         System.out.println("Paco ahora tiene " + p1.getDinero());
         System.out.println("Juan ahora tiene " + p2.getDinero());
+        */
 
+
+
+        /*"Ejercicio Par o Impar."
+
+       Scanner keyboard = new Scanner(System.in);
+        System.out.println("Inserte un número:");
+       int valor = keyboard.nextInt();
+
+
+
+       if (valor%2 == 0){
+           System.out.println("El número introducido es par.");
+       }
+
+       else {
+           System.out.println("El número introducido es impar");
+       }
+        */
+
+
+
+        //Ejercicio adivinar número en tres intentos.
+
+
+      /*  Random generator = new Random();
+        int random = generator.nextInt(10) + 1;
+
+        for (int numIn = 0; numIn <= 2; numIn += 1) {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Inserta número: ");
+        int valor = keyboard.nextInt();
+
+            if (valor == random) {
+                System.out.println("Es correcto!!");
+            break;
+            }
+
+            if (valor < random) {
+                System.out.println("Más alto!");
+            } else {
+                System.out.println("Menos!!");
+            }
         }
+        */
+
 
 
     }
+}
 
 
 
